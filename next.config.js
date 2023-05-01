e9be7064b87aac947f0450b037995c8b1b67e8de
DimiHK <dimihtsk@gmail.com>
2023-05-01 16:28:52 +0100

    added image otimization package

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
   images: {
     deviceSizes: [640, 750, 828, 1080],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     domains: ["https://dimihk.github.io/"],
   },
-};
+  reactStrictMode: true,
+});
