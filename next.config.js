16c2f6ca6f491222578650b88732a0bf88ea262e
DimiHK <dimihtsk@gmail.com>
2023-05-07 18:54:32 +0100

    modified config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+
+  pageExtensions: [
+    "useBrakepoints.tsx",
+    "useBrakepoints.ts",
+    "useBrakepoints.jsx",
+    "useBrakepoints.js",
+  ],
 };
